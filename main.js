c251ba88c53ba2e2b78e7638e67caaec7ce64e42
Lewis Murray <lewisdmurray00@gmail.com>
2021-07-15 22:50:26 +0100

    Added the storyline for the kata

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 /* 
-👉 Write your kata here!
+👉 
+You are working in an office and the company keeps a database
+on the list of customers that you have, the database contains
+the clients email, age, D.O.B, notes and finally their 
+phone numbers.
+
+Your boss has asked if you can hide the clients phone numbers
+due to security reason
+
+You must do this by replacing all digits of the number with
+the letter "X"
+
+For example: "XXXXX XXXXXX"
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
